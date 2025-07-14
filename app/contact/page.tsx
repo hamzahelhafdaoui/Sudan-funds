015ffc78fabbcf47760ef8adb292582e28e597d4
Hamzah Elhafdaoui <69404894+hamzahelhafdaoui@users.noreply.github.com>
2025-07-14 19:07:29 -0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,8 +3,8 @@ import { Shield } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 
 export default function ContactPage() {
@@ -48,28 +48,32 @@ export default function ContactPage() {
                   </CardDescription>
                 </CardHeader>
                 <CardContent>
-                  <form className="grid gap-4">
+                  <form
+                    action="https://formspree.io/f/mnnzakbv"
+                    method="POST"
+                    className="space-y-4"
+                  >
                     <div className="grid grid-cols-2 gap-4">
                       <div className="space-y-2">
                         <Label htmlFor="first-name">First name</Label>
-                        <Input id="first-name" placeholder="Enter your first name" />
+                        <Input id="first-name" name="first-name" placeholder="Enter your first name" required />
                       </div>
                       <div className="space-y-2">
                         <Label htmlFor="last-name">Last name</Label>
-                        <Input id="last-name" placeholder="Enter your last name" />
+                        <Input id="last-name" name="last-name" placeholder="Enter your last name" required />
                       </div>
                     </div>
                     <div className="space-y-2">
                       <Label htmlFor="email">Email</Label>
-                      <Input id="email" placeholder="Enter your email" type="email" />
+                      <Input id="email" name="email" type="email" placeholder="Enter your email" required />
                     </div>
                     <div className="space-y-2">
                       <Label htmlFor="subject">Subject</Label>
-                      <Input id="subject" placeholder="Enter the subject" />
+                      <Input id="subject" name="subject" placeholder="Enter the subject" required />
                     </div>
                     <div className="space-y-2">
                       <Label htmlFor="message">Message</Label>
-                      <Textarea className="min-h-[120px]" id="message" placeholder="Enter your message" />
+                      <Textarea id="message" name="message" className="min-h-[120px]" placeholder="Enter your message" required />
                     </div>
                     <Button className="bg-red-600 hover:bg-red-700" type="submit">
                       Send Message
